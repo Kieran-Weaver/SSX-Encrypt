671910023524ad684cffb7c4008768ef3bd9eda9
Kieran Weaver <drawbook.productions@gmail.com>
2017-02-24 21:28:46 -0800

    Fixed

--- insert_title.py
+++ insert_title.py
@@ -1,3 +1,8 @@
-def apply_cipher(plaintext):
+import random
+def apply_cipher(plaintext,key):
     print("I don't feel like doing this today")
-apply_cipher("doge")
+    random.seed(a=key)
+    s2 = "".join([chr(random.randrange(255)) for i in range(len(plaintext))])
+    print(''.join(chr(ord(a) ^ ord(b)) for a,b in zip(plaintext,s2)))
+    print([ord(a) ^ ord(b) for a,b in zip(plaintext,s2)])
+apply_cipher("doge",21)
