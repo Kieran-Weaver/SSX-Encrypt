a2b752b20b4f4f4bd83459f18f36c963bf0b8ddd
Kieran Weaver <drawbook.productions@gmail.com>
2017-02-25 09:04:28 -0800

    Fixed

--- insert_title.py
+++ insert_title.py
@@ -4,5 +4,8 @@ def apply_cipher(plaintext,key):
     random.seed(a=key)
     s2 = "".join([chr(random.randrange(255)) for i in range(len(plaintext))])
     print(''.join(chr(ord(a) ^ ord(b)) for a,b in zip(plaintext,s2)))
-    print([ord(a) ^ ord(b) for a,b in zip(plaintext,s2)])
+    print([dafunction(ord(a),ord(b)) for a,b in zip(plaintext,s2)])
 apply_cipher("doge",21)
+
+def dafunction(plaintext1,key1):
+    return plaintext1 ^ key1
